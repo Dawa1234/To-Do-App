132fb232735a1c18df703534df8ef953d91c33bd
Dawa1234 <sdawajangbu99@gmail.com>
2023-04-17 16:49:46 +0545

    firebase initial configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:todo/logic/bloc_exports.dart';
@@ -7,5 +8,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HydratedBloc.storage = await HydratedStorage.build(
       storageDirectory: await getApplicationDocumentsDirectory());
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
